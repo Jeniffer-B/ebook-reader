215e0e8ff326c2a6f0e6af9c12faa94fa6462df2
Jeniffer-B <69005661+Jeniffer-B@users.noreply.github.com>
2022-02-07 17:32:46 +0100

    fixed getting totalResults

--- store/index.js
+++ store/index.js
@@ -1,29 +1,42 @@
 export const state = () => ({
-  bookSearchQuery: '',
-  bookSearchResults: null
+  bookSearchQuery: null,
+  bookSearchResults: null,
+  totalResults: []
 })
 
 export const mutations = {
   SET_BOOK_SEARCH_QUERY (state, book) {
     state.bookSearchQuery = book
   },
-  SET_BOOK_SEARCH_RESULTS(state, results) {
+  SET_BOOK_SEARCH_RESULTS (state, results) {
     state.bookSearchResults = results
+  },
+  SET_TOTAL_BOOKS_RESEARCH (state, totalResults) {
+    state.totalResults = totalResults
   }
 }
 export const getters = {
   bookSearchResults: state => state.bookSearchResults
 }
 export const actions = {
-  async getBooksByTitle ({commit, state}) {
-    let books
+  async getBooksByTitle ({ commit, state }) {
+    if (state.bookSearchQuery === null) {
+      console.error('search query is empty')
+      return
+    }
+    let results
+    let totalResults
     fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${state.bookSearchQuery}`)
       .then(response => response.json())
-      .then(response => {
-        books = response.items.map(book => book.volumeInfo)
-        commit('SET_BOOK_SEARCH_RESULTS', books)
-      }
-    );          
-    
+      .then((response) => {
+        totalResults = response.totalItems
+        if (totalResults === 0) {
+          window.alert(`No hay resultados buscando por: "${state.bookSearchQuery}"`)
+          results = []
+        } else {
+          results = response.items.map(book => book.volumeInfo)
+        }
+        commit('SET_BOOK_SEARCH_RESULTS', results)
+      })
   }
 }
